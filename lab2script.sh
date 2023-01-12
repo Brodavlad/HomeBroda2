dd71a71a97aa4561b2a872ca1ceb9479a4e01e3c
Brodavlad <75326742+Brodavlad@users.noreply.github.com>
2023-01-12 03:19:37 +0200

    Update lab2script.sh

--- lab2script.sh
+++ lab2script.sh
@@ -1,41 +1,58 @@
 #!/bin/bash
 
 ## create vpc (not default!!) (put 10.0.0.., not 10.10.0..)
-aws ec2 create-vpc --cidr-block 10.0.0.0/16
+VPS_ID=`aws ec2 create-vpc --cidr-block 10.0.0.0/16 --query Vpc.VpcId --output text`
 
 ## creating subnets
 ##public
-aws ec2 create-subnet --vpc-id vpc-00a67537573b9bd10 --cidr-block 10.0.1.0/24
-aws ec2 create-tags --resources subnet-01a1d5bd8ff785c90 --tags Key=public,Value=public
+SUBNET_PUBLIC_ID=`aws ec2 create-subnet --vpc-id $VPS_ID --cidr-block 10.0.1.0/24 --query Subnet.SubnetId --output text`
+##tag subnet as public
+aws ec2 create-tags --resources $SUBNET_PUBLIC_ID --tags Key=public,Value=public 
 ##private
-aws ec2 create-subnet --vpc-id vpc-00a67537573b9bd10 --cidr-block 10.0.2.0/24
-aws ec2 create-tags --resources subnet-04b09689bbb7d5f0a --tags Key=private,Value=private
+SUBNET_PRIVATE_ID=`aws ec2 create-subnet --vpc-id $VPS_ID --cidr-block 10.0.2.0/24 --query Subnet.SubnetId --output text`
+##tag subnet as private 
+aws ec2 create-tags --resources $SUBNET_PRIVATE_ID --tags Key=private,Value=private 
 
-##internet gateway
-aws ec2 create-internet-gateway
+## create internet gateway, to make subnet as public subnet
+GATEWAY_ID=`aws ec2 create-internet-gateway --query InternetGateway.InternetGatewayId --output text`
 
 ##attach gateway to net1
-aws ec2 attach-internet-gateway --internet-gateway-id igw-0f7b76eedde6cfc73 --vpc-id vpc-065ce59e9d41ce119
+aws ec2 attach-internet-gateway --vpc-id $VPC_ID --internet-gateway-id $GATEWAY_ID 
 
-##create ellastic ip
-aws ec2 allocate-address --domain vpc
-##connecting nat to elastic ip
-aws ec2 create-nat-gateway --subnet-id subnet-01a1d5bd8ff785c90 --allocation-id eipalloc-097b1ba265e0afdce
+##create custom route table
+ROUTE_ID=`aws ec2 create-route-table --vpc-id $VPS_ID --query RouteTable.RouteTableId --output text`
+## and update it (route table)
+aws ec2 create-route --route-table-id $ROUTE_ID --destination-cidr-block 0.0.0.0/0 --gateway-id $GATEWAY_ID
 
-##create route table
-aws ec2 create-route-table --vpc-id vpc-00a67537573b9bd10
+## associate subnet using custom route table to make it as public 
+aws ec2 associate-route-table  --subnet-id $SUBNET_PUBLIC_ID --route-table-id $ROUTE_ID
 
-##tag the first route table as public
-aws ec2 create-tags --resources rtb-0c367934250368a2b --tags Key=public,Value=public
+## configure subnet to give a public IP to EC2 instances
+aws ec2 modify-subnet-attribute --subnet-id $SUBNET_PUBLIC_ID --map-public-ip-on-launch
 
-##creating a routes for routes table
-aws ec2 create-route --route-table-id rtb-0c367934250368a2b --destination-cidr-block 0.0.0.0/0 --gateway-id igw-0481d6693b68a26ea
+## create a key pair and output
+aws ec2 create-key-pair --key-name cli-keyPair --query 'KeyMaterial' --output text > ./cli-keyPair.pem
 
-##associate route table to subnet
-aws ec2 associate-route-table --route-table-id rtb-0c367934250368a2b --subnet-id subnet-01a1d5bd8ff785c90
+## Modify permissions (change acces from modified keypair)
+chmod 400 cli-keyPair.pem
 
+#№ create security group with acces to SSH
+GROUP_ID=`aws ec2 create-security-group --group-name SSHAccess --description "security group for ssh " --vpc-id $VPS_ID --query GroupId --output text`
 
-##create key pair
-aws ec2 create-key-pair --key-name cli-keyPair --query 'KeyMaterial' --output text > cli-keyPair.pem
+## configure security group for tcp port 22
+aws ec2 authorize-security-group-ingress --group-id $GROUP_ID --protocol tcp --port 22 --cidr 0.0.0.0/0
 
-##create ec2 instance
+## launch instance in public subnet
+INSTANCE_ID=`aws ec2 run-instances --image-id ami-0533f2ba8a1995cf9 --count 1 --instance-type t2.micro --key-name MyKeyPair --security-group-ids $GROUP_ID --subnet-id $SUBNET_PUBLIC_ID --query Instances[0].InstanceId --output text`
+aws ec2 modify-vpc-attribute --enable-dns-hostnames --vpc-id $VPS_ID 
+
+## Retrieve IP address
+IP_ADDRESS=`aws ec2 describe-instances --instance-ids $INSTANCE_ID --query Reservations[].Instances[].PublicDnsName --output text`
+
+## connect to instance with key pair and public IP
+ssh -i "cli-keyPair.pem" ec2-user@$IP_ADDRESS
+
+## check instance status
+aws ec2 describe-instance-status --instance-id $INSTANCE_ID
+
+sleep 40
