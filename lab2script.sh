02941126db38f48e2896c9323702c1cea6a667c0
Brodavlad <75326742+Brodavlad@users.noreply.github.com>
2023-01-14 17:32:24 +0200

    Update lab2script.sh

--- lab2script.sh
+++ lab2script.sh
@@ -31,10 +31,10 @@ aws ec2 associate-route-table  --subnet-id $SUBNET_PUBLIC_ID --route-table-id $R
 aws ec2 modify-subnet-attribute --subnet-id $SUBNET_PUBLIC_ID --map-public-ip-on-launch
 
 ## create a key pair and output
-aws ec2 create-key-pair --key-name cli-keyPair --query 'KeyMaterial' --output text > ./cli-keyPair.pem
+aws ec2 create-key-pair --key-name keyBroda --query 'KeyMaterial' --output text > ./keyBroda.pem
 
 ## Modify permissions (change acces from modified keypair)
-chmod 400 cli-keyPair.pem
+chmod 400 keyBroda.pem
 
 #№ create security group with acces to SSH
 GROUP_ID=`aws ec2 create-security-group --group-name SSHAccess --description "security group for ssh " --vpc-id $VPS_ID --query GroupId --output text`
@@ -43,14 +43,14 @@ GROUP_ID=`aws ec2 create-security-group --group-name SSHAccess --description "se
 aws ec2 authorize-security-group-ingress --group-id $GROUP_ID --protocol tcp --port 22 --cidr 0.0.0.0/0
 
 ## launch instance in public subnet
-INSTANCE_ID=`aws ec2 run-instances --image-id ami-0b5eea76982371e91 --count 1 --instance-type t2.micro --key-name cli-keyPair --security-group-ids $GROUP_ID --subnet-id $SUBNET_PUBLIC_ID --query Instances[0].InstanceId --output text`
+INSTANCE_ID=`aws ec2 run-instances --image-id ami-0b5eea76982371e91 --count 1 --instance-type t2.micro --key-name keyBroda --security-group-ids $GROUP_ID --subnet-id $SUBNET_PUBLIC_ID --query Instances[0].InstanceId --output text`
 aws ec2 modify-vpc-attribute --enable-dns-hostnames --vpc-id $VPS_ID 
 
 ## Retrieve IP address
 IP_ADDRESS=`aws ec2 describe-instances --instance-ids $INSTANCE_ID --query Reservations[].Instances[].PublicDnsName --output text`
 
 ## connect to instance with key pair and public IP
-ssh -i "cli-keyPair.pem" ec2-user@$IP_ADDRESS
+ssh -i "keyBroda.pem" ec2-user@$IP_ADDRESS
 
 ## check instance status
 aws ec2 describe-instance-status --instance-id $INSTANCE_ID
